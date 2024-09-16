b4f1a085250261148f36ee4d95b7f5d2ed1a8847
Xman <admin@user.vn>
2024-09-16 09:47:17 +0700

    wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -7,7 +7,8 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>{{ config('app.name') }}</title>
+        <title>Check var mttq, Check bill ủng hộ miền bắc, check bill mttq</title>
+        <meta name="description" content="Check var mttq, Check bill ủng hộ miền bắc, check bill mttq">
 
         <style>
             [x-cloak] {
--- resources/views/livewire/sao-ke2024.blade.php
+++ resources/views/livewire/sao-ke2024.blade.php
@@ -3,7 +3,7 @@
     <h1 class="text-2xl font-extrabold">
         Sao Kê VAR 2024 🕵️‍♂️
     </h1>
-    <p>Lưu ý:  Dữ liệu cung cấp bởi <a href="https://www.facebook.com/thongtinchinhphu/posts/pfbid03YkRTKZ5WjeHwBavPQbP7EShonj9tTExgY26gNhvQdiEsbjdsLWnzWEoQE1bU9SBl" target="_blank" rel="noopener noreferrer nofollow" class="font-bold">MTTQ</a>, web bởi <a href="https://interdata.vn" target="_blank" class="font-bold">INTERDATA.VN</a></p>
+    <p>Lưu ý:  Dữ liệu cung cấp bởi <a href="https://www.facebook.com/thongtinchinhphu/posts/pfbid03YkRTKZ5WjeHwBavPQbP7EShonj9tTExgY26gNhvQdiEsbjdsLWnzWEoQE1bU9SBl" target="_blank" rel="noopener noreferrer nofollow" class="font-bold">MTTQ</a>,  <a href="https://interdata.vn/cloud-server" target="_blank" class="font-bold">Cloud Server</a> và <a href="https://interdata.vn/thue-vps" target="_blank" class="font-bold">VPS</a> được cung cấp bởi <a href="https://interdata.vn" target="_blank" class="font-bold">INTERDATA.VN</a></p>
     <ul>
         <li>- <a href="https://www.facebook.com/mttqvietnam/posts/pfbid0YSaZTjEw2GBMnT5bNBi49djNxnxi326VjKodHzdxvhpkW3rwTs8u5dCeVGvQmU18l" target="_blank" rel="noopener noreferrer nofollow">Số tiền ủng hộ qua số tài khoản Vietcombank 0011001932418 từ ngày 1/9/2024 đến ngày 10/9/2024</a></li>
         <li>- <a href="https://www.facebook.com/mttqvietnam/posts/pfbid0cGpdUA8JFuB5TPLFX5E1GmhBSkYs99v9rRLY5sY2mTD1pJ16Cq1BvtkrSXLfnsESl" target="_blank" rel="noopener noreferrer nofollow">Số tiền ủng hộ qua số tài khoản Vietinbank CT1111 từ ngày 10/9/2024 đến ngày 12/9/2024</a></li>
